dc65dba41113d7b9d6653759dd5b10e7ac68aac8
Mario Marroquin <mmarroking@gmail.com>
2017-12-27 22:46:46 -0800

    Calculate distance from origin to each bike station

--- apiTest.js
+++ apiTest.js
@@ -1,6 +1,14 @@
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
+const googleAPI = require("./googleApi");
 
-fetch('http://santamonicabikeshare.com/opendata/station_information.json')
-  .then(function(response){return response.json()})
-  .then(function(response){console.log(response.data.stations[1])})
-
+fetch("http://santamonicabikeshare.com/opendata/station_information.json")
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(response) {
+    var data = [];
+    response.data.stations.forEach(function(station) {
+      data.push(station.lat + "," + station.lon);
+    });
+    googleAPI(data);
+  });
--- googleApi.js
+++ googleApi.js
@@ -1,14 +1,26 @@
-const fetch = require('node-fetch');
-require('dotenv').config()
+const fetch = require("node-fetch");
+require("dotenv").config();
 
-var origin = ''
-var destination = ''
-var apiKey = process.env.GOOGLE_MAP_API_KEY;
+module.exports = function(data) {
+  var origin = "9245+W+Venice+Blvd+Los+Angeles+CA+90034";//random address
+  var destination = data.join("|");
+  var apiKey = process.env.GOOGLE_MAP_API_KEY;
 
-fetch('https://maps.googleapis.com/maps/api/distancematrix/json?origins='+origin+'&destinations='+destination+'&key='+apiKey+'&mode=walking')
-  .then(function(response){return response.json()})
-  .then(function(response){response.rows.forEach(
-    function(x){console.log(x.elements)}
-  )})
-
-
+  fetch(
+    "https://maps.googleapis.com/maps/api/distancematrix/json?origins=" +
+      origin +
+      "&destinations=" +
+      destination +
+      "&key=" +
+      apiKey +
+      "&mode=walking"
+  )
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(response) {
+      response.rows.forEach(function(result) {
+        console.log(result.elements);
+      });
+    });
+};
