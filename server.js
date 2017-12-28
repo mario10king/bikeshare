427b344e2c67c4b92d4cd5655bceb94536cb3176
Mario Marroquin <mmarroking@gmail.com>
2017-12-28 11:02:09 -0800

    Redirect page after form submission

--- server.js
+++ server.js
@@ -2,5 +2,6 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => res.sendFile('index.html',  {root: __dirname }))
+app.post('/', (req, res) => res.sendFile('map.html',  {root: __dirname }))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
