2e595ebad0e163da7eb99e45ee3aa84752596dad
Mario Marroquin <mmarroking@gmail.com>
2017-12-28 03:13:45 -0800

    Find the closest station to destination

--- apiTest.js
+++ apiTest.js
@@ -1,20 +1,41 @@
 const fetch = require("node-fetch");
 const googleAPI = require("./googleApi");
 
-fetch("http://santamonicabikeshare.com/opendata/station_information.json")
+function controller(origin, destination){
+  getStationsInfo.then(function(info){
+    var formattedStations = formatStations(info)
+    googleAPI(origin, destination, formattedStations).then(function(ids){
+      console.log(getAddresses(ids, info))
+    })
+  })
+}
+
+var getStationsInfo = fetch("http://santamonicabikeshare.com/opendata/station_information.json")
   .then(function(response) {
     return response.json();
   })
   .then(function(response) {
-    var data = [];
-    response.data.stations.forEach(function(station) {
-      data.push({id: station.station_id, address: station.lat + "," + station.lon});
+    return response
+  })
+
+function getAddresses(ids, info){
+   var originStation;
+   var destinationStation;
+   info.data.stations.forEach(function(station) {
+     if(ids[0] === station.station_id){
+       originStation = station.address
+     }
+     if(ids[1] === station.station_id){
+       destinationStation = station.address
+     }
+   });
+   return [originStation, destinationStation]
+}
+
+function formatStations(info){
+    return info.data.stations.map(function(station) {
+      return {id: station.station_id, address: station.lat + "," + station.lon};
     });
-    googleAPI(data).then(function (id){
-      response.data.stations.forEach(function(station) {
-        if(id === station.station_id){
-          console.log(station)
-        }
-      });
-     })
-   })
+}
+
+controller("242 25th St Santa Monica CA 90402", "1210 Ozone Ave Santa Monica CA 90405")//random addresses
--- googleApi.js
+++ googleApi.js
@@ -1,8 +1,9 @@
 const fetch = require("node-fetch");
 require("dotenv").config();
 
-module.exports = function(data) {
-  var origin = "9245+W+Venice+Blvd+Los+Angeles+CA+90034";//random address
+module.exports = function(origin, finalDestination, data) {
+  var origin = origin.split(" ").join("+");
+  var finalDestination= finalDestination.split(" ").join("+");
   var newData = data.slice(15,25) //slice to get a subset of data to not go over api limit for testing
   var destination = newData.map(function(station){ 
     return station.address
@@ -11,7 +12,7 @@ module.exports = function(data) {
 
   return fetch(
     "https://maps.googleapis.com/maps/api/distancematrix/json?origins=" +
-      origin +
+      origin + "|" + finalDestination +
       "&destinations=" +
       destination +
       "&key=" +
@@ -35,6 +36,21 @@ module.exports = function(data) {
         }
      } 
      });
-     return(newData[shortestElement].id)
+var shortestDestination;
+      var shortestElementDestination;
+      response.rows[1].elements.forEach(function(result, index) { 
+      if(result.status === 'OK'){
+        if (shortestDestination === undefined){
+          shortestDestination = result.duration.value 
+          shortestElementDestinaion = index
+        }else if(result.duration.value < shortestDestination){
+          shortestDestination = result.duration.value 
+          shortestElementDestination = index 
+        }
+     } 
+     });
+     var closestOrigin = newData[shortestElement].id
+     var closestDestination = newData[shortestElementDestination].id 
+     return([closestOrigin, closestDestination])
     });
 };
