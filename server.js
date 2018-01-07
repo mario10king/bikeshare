9fc371eb1f15d6da338106d546bab5fc2d7dfb8b
Mario Marroquin <mmarroking@gmail.com>
2018-01-06 17:00:02 -0800

    Set Up templating engine

--- server.js
+++ server.js
@@ -2,16 +2,20 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const controller = require("./apiTest")
 const app = express()
+const cons = require('consolidate')
+const handlebars = require('handlebars')
 
 app.use(bodyParser.urlencoded())
+app.engine('html', cons.handlebars)
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
 
 app.get('/', (req, res) => res.sendFile('index.html',  {root: __dirname }))
 
 app.post('/', function(req, res) {
   controller(req.body.origin, req.body.destination).then(
   function(response){
-    console.log(response)
-    //res.render('map.html',  {origin: response[0], destination: response[1], root: __dirname })
+    res.render('test.html',  {origin: response[0], destination: response[1], root: __dirname })
   })
 })
 
