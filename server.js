4afac2c516790e65623d42f1ce987b6997cdda1d
Mario Marroquin <mariomarroquin@marios-mbp.attlocal.net>
2018-08-06 07:50:45 -0700

    Fixed server to accept differnt PORTS

--- server.js
+++ server.js
@@ -30,4 +30,4 @@ app.post('/', function(req, res) {
   });
 });
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port 3000!'));
